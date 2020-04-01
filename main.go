77b620d216942a65585af7eb5dca75e913a7238e
kembo91 <ketema.galy@gmail.com>
2020-04-01 16:02:44 +0300

    added go backend

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/handlers"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	api := r.PathPrefix("/api").Subrouter()
+	api.HandleFunc("/msg", MessageHandler).Methods("POST")
+
+	buildHandler := http.FileServer(http.Dir("./build"))
+	r.PathPrefix("/").Handler(buildHandler)
+	srv := http.Server{
+		Handler:      handlers.LoggingHandler(os.Stdout, r),
+		Addr:         "127.0.0.1:8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
+}
+
+func MessageHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Can't read body", err)
+		http.Error(w, "Can't read body", http.StatusBadRequest)
+	}
+	log.Print(body)
+}
